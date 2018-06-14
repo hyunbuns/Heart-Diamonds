d6d3af9a0b355b387367fad59b55838cc12f2ec3
Hyun Ko <36653734+hyunbuns@users.noreply.github.com>
2018-06-14 10:54:59 +0900

    Add files via upload

---
+++ Heart_Diamond.java
@@ -0,0 +1,38 @@
+package Data_Structures;
+
+public class Heart_Diamond {
+	public static void main(String[] args) {
+	
+	  for(int top = 0; top<1; top++) {	
+		  System.out.println("    ♡");
+		for(int top2 = -1; top2<0; top2++) {
+			System.out.println("   ♡♡");	
+			for(int top3 = -2; top3<-1; top3++) {
+				System.out.println("  ♡♡♡");
+				for(int top4 = -3; top4<-2; top4++) {
+					System.out.println("♡♡♡♡♡");
+			}
+		}
+	} }
+
+		for(int bottom = 0; bottom<1; bottom++) {
+			System.out.println("♡♡♡♡♡");
+			for(int bottom2 = -1; bottom2<0; bottom2++) {
+				System.out.println("  ♡♡♡");
+			}
+			  for(int bottom3 = -2; bottom3<-1; bottom3++) {
+				  System.out.println("   ♡♡");
+			  }
+			  	for(int bottom4 = -3; bottom4<-2; bottom4++) {
+			  		System.out.println("    ♡");
+			  	}
+		}
+		
+		
+
+		
+		
+		
+		
+	}
+}
